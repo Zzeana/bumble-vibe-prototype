91cb93e00d87cdc082bcfc73ecf70b0a2526283d
zaynaa <zee7@pitt.edu>
2026-02-21 08:30:27 -0500

    Removed Cursor-specific files for Claude prototype

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { SidequestAnnouncement } from "./SidequestAnnouncement/SidequestAnnouncement";
-export { EnableLocation } from "./EnableLocation/EnableLocation";
-export { TopBar } from "./TopBar/TopBar";
-export { BenefitItem } from "./BenefitItem/BenefitItem";
-export { Button } from "./Button/Button";
-export { SidequestIllustration } from "./SidequestIllustration/SidequestIllustration";
-export { LocationIllustration } from "./LocationIllustration/LocationIllustration";
